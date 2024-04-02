b53cd1e424559523b40b6193515a7de173364358
webofceco <mapleselijah9@gmail.com>
2024-04-02 00:52:39 -0400

    updated docstrings to include tbody functions

--- src/HTMLTables.jl
+++ src/HTMLTables.jl
@@ -1,10 +1,15 @@
 """
     HTMLTables 
 
-Read and write HTML tables.
+Julia package for reading and writing HTML tables.
 
+## Reading HTML tables
 - `HTMLTables.read`: read HTML table into a `DataFrame`.
+- `HTMLTables.readtbody`: read the HTML <tbody></tbody> element into a `DataFrame`.
+
+## Writing HTML tables
 - `HTMLTables.write`: write a `DataFrame` into an HTML table.
+- `HTMLTables.writetbody`: write a `DataFrame` into an HTML <tbody></tbody> element.
 """
 module HTMLTables
 
