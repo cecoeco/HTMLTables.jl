38184e8b1642cae565ca697d91d7b70d2d4fcd9a
webofceco <mapleselijah9@gmail.com>
2024-04-03 05:53:31 -0400

    updated docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,28 @@
 using Documenter
+using DocumenterCitations
 using HTMLTables
+
+bib_filepath = Base.joinpath(Base.dirname(@__FILE__), "src/assets/references.bib")
+bib = DocumenterCitations.CitationBibliography(bib_filepath, style=:authoryear)
 
 Documenter.makedocs(
     modules=[HTMLTables],
+    format=Documenter.HTML(
+        assets=[
+            "src/assets/bib.css"
+        ]
+    ),
     pages=[
         "Home" => "index.md",
         "Reading" => "reading.md",
         "Writing" => "writing.md",
         "Examples" => "examples.md",
+        "References" => "references.md"
     ],
     repo="https://github.com/cecoeco/HTMLTables.jl",
     sitename="HTMLTables.jl",
     authors="Ceco Elijah Maples",
-    warnonly=Documenter.except(),
+    plugins=[bib]
 )
 
 Documenter.deploydocs(repo="github.com/cecoeco/HTMLTables.jl.git")
