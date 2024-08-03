a94d0efc273986795d8a9799b34fcc025d5b7e67
webofceco <mapleselijah9@gmail.com>
2024-08-03 14:05:46 -0400

    fixed docs, added random module

--- docs/make.jl
+++ docs/make.jl
@@ -1,27 +1,22 @@
-using Documenter
-using DocumenterCitations
+using Documenter: makedocs, HTML, deploydocs
+using DocumenterCitations: CitationBibliography
 using HTMLTables
 
-bib_filepath = Base.joinpath(Base.dirname(Base.@__FILE__), "src/assets/references.bib")
-bib = DocumenterCitations.CitationBibliography(bib_filepath, style=:authoryear)
+pages::Vector{Pair{String,String}} = [
+    "Home" => "index.md",
+    "Reading" => "reading.md",
+    "Writing" => "writing.md",
+    "References" => "references.md"
+]
 
-Documenter.makedocs(
+makedocs(
     modules=[HTMLTables],
-    format=Documenter.HTML(
-        assets=[
-            "src/assets/bib.css"
-        ]
-    ),
-    pages=[
-        "Home" => "index.md",
-        "Reading" => "reading.md",
-        "Writing" => "writing.md",
-        "References" => "references.md"
-    ],
+    format=HTML(assets=["src/assets/bib.css"]),
+    pages=pages,
     repo="https://github.com/cecoeco/HTMLTables.jl",
     sitename="HTMLTables.jl",
     authors="Ceco Elijah Maples",
-    plugins=[bib]
+    plugins=[CitationBibliography(joinpath(dirname(@__FILE__), "src/assets/references.bib"), style=:authoryear)]
 )
 
-Documenter.deploydocs(repo="github.com/cecoeco/HTMLTables.jl.git")
+deploydocs(repo="github.com/cecoeco/HTMLTables.jl.git")
