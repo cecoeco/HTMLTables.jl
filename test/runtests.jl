e701bf60bf24ba4454eaad35af61a7ba31b42bfd
webofceco <mapleselijah9@gmail.com>
2024-08-22 19:24:55 -0400

    updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,25 @@
-using Test, HTMLTables, DataFrames
+module TestHTMLTables
 
-const CSS_DIR::String = joinpath(dirname(@__FILE__), "css")
-const HTML_DIR::String = joinpath(dirname(@__FILE__), "html")
+using CSV
+using DataFrames
+using DataStructures
+using HTMLTables
+using HTTP
+using JSON3
+using JSONTables
+using Statistics
+using Random
+using Tables
+using Test
+using XLSX
+
+const CSS_DIR::String = Base.Filesystem.joinpath(
+    Base.Filesystem.dirname(Base.@__FILE__), "css"
+)
+
+const HTML_DIR::String = Base.Filesystem.joinpath(
+    Base.Filesystem.dirname(Base.@__FILE__), "html"
+)
 
 include("read/file.jl")
 include("read/http.jl")
@@ -16,3 +34,5 @@ include("write/io.jl")
 include("write/string.jl")
 include("write/table.jl")
 include("write/write.jl")
+
+end
