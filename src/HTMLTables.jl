11f7e212c129701b26fe688e75011e61797cff83
webofceco <mapleselijah9@gmail.com>
2024-11-01 21:59:49 -0400

    simplify

--- src/HTMLTables.jl
+++ src/HTMLTables.jl
@@ -5,8 +5,8 @@ Julia package for reading and writing HTML tables using the Tables.jl interface
 
 ## exported functions:
 
-- `HTMLTables.readtable`: reads an HTML table into a Tables.jl-compatible table like a `DataFrame`.
-- `HTMLTables.writetable`: converts a Tables.jl-compatible table to an HTML table and writes it to a file or IO stream.
+- `readtable`: reads an HTML table into a Tables.jl-compatible table like a `DataFrame`.
+- `writetable`: converts a Tables.jl-compatible table to an HTML table and writes it to a file or IO stream.
 
 """
 module HTMLTables
